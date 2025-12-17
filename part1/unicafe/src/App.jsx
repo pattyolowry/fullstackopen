c230537c642dc3751fe89107ecc786823134a092
Patrick Lowry <patio.lowry@gmail.com>
2025-12-17 12:51:45 -0800

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,21 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  return (
+    <div>
+      <h2>statistics</h2>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}<br/>
+      all {total}<br/>
+      average {(good - bad) / total}<br/>
+      positive {good / total * 100} %
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,13 +33,7 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <h2>statistics</h2>
-      good {good}<br/>
-      neutral {neutral}<br/>
-      bad {bad}<br/>
-      all {good + neutral + bad}<br/>
-      average {(good - bad) / (good + neutral + bad)}<br/>
-      positive {good / (good + neutral + bad) * 100} %
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
