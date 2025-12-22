fc4f0668e4800a029a75711cce0e2c9c015706f1
Patrick Lowry <patio.lowry@gmail.com>
2025-12-21 22:40:10 -0800

    exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -40,12 +40,16 @@ const App = () => {
     const personObject = {
       name: newName,
       phone: newPhone,
-      id: String(persons.length + 1)
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewPhone('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        console.log(response)
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewPhone('')
+      })
   }
 
   const handleNameChange = (event) => {
