729cd80d489e93c2142a7b6fe549cf7ab6ba4b46
Patrick Lowry <patio.lowry@gmail.com>
2025-12-26 08:55:56 -0800

    exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,12 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token("request-body", (req) => JSON.stringify(req.body))
+
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :request-body'
+))
 
 let persons = [
     { 
