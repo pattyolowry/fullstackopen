92a88737006597d892d4bfb55b8cb82b81c1ad99
Patrick Lowry <patio.lowry@gmail.com>
2026-01-06 21:33:16 -0500

    exercise 4.13

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -123,6 +123,29 @@ test('creating blog without url returns 400 error', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const blogToDelete = blogsAtStart.body[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  assert.strictEqual(blogsAtEnd.body.length, blogsAtStart.body.length - 1)
+
+  const ids = blogsAtEnd.body.map(b => b.id)
+  assert(!ids.includes(blogToDelete.id))
+})
+
+test('delete with invalid id returns 400', async () => {
+  const invalidId = '1234'
+
+  await api
+    .delete(`/api/blogs/${invalidId}`)
+    .expect(400)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
