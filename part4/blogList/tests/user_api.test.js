c9553529fb7e1daff3a27f322e8fb9789d0f1909
Patrick Lowry <patio.lowry@gmail.com>
2026-01-08 01:08:16 -0500

    exercise 4.15

---
+++ part4/blogList/tests/user_api.test.js
@@ -0,0 +1,61 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await User.deleteMany({})
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+  await user.save()
+})
+
+test('user creation succeeds with fresh username', async () => {
+  const usersAtStart = await api.get('/api/users')
+  const newUser = {
+    username: 'testuser',
+    name: 'Test User',
+    password: 'foobar'
+  }
+
+  await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const usersAtEnd = await api.get('/api/users')
+  assert.strictEqual(usersAtEnd.body.length, usersAtStart.body.length + 1)
+
+  const usernames = usersAtEnd.body.map(u => u.username)
+  assert(usernames.includes(newUser.username))
+})
+
+test('user creation fails if username already taken', async () => {
+  const usersAtStart = await api.get('/api/users')
+
+  const newUser = {
+    username: 'root',
+    name: 'Superuser',
+    password: '123456'
+  }
+
+  const response = await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(400)
+
+  assert(response.body.error.includes('expected `username` to be unique'))
+
+  const usersAtEnd = await api.get('/api/users')
+  assert.strictEqual(usersAtEnd.body.length, usersAtStart.body.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
