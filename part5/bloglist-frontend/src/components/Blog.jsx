badc249c85a5fbf93c61653a5552bb6bda37b162
Patrick Lowry <patio.lowry@gmail.com>
2026-01-09 23:00:16 -0500

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleDetails = () => {
+    setVisible(!visible)
+  }
+
+  const buttonText = () => {
+    return visible ? 'Hide' : 'View'
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  console.log(blog)
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author} <button onClick={toggleDetails}>{buttonText()}</button>
+      <div style={showWhenVisible}>
+        {blog.url}<br/>
+        Likes {blog.likes}<br/>
+        Added by {blog.user.name}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
