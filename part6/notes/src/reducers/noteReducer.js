389bed5ba8e36340e0b7e86b84b649016f545b6e
Patrick Lowry <patio.lowry@gmail.com>
2026-01-27 20:52:56 -0500

    part 6c notes continued

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { current } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
@@ -26,6 +27,22 @@ const noteSlice = createSlice({
   },
 })
 
+const { createNote, setNotes } = noteSlice.actions
 
-export const { createNote, toggleImportanceOf, setNotes } = noteSlice.actions
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const appendNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(createNote(newNote))
+  }
+}
+
+
+export const { toggleImportanceOf } = noteSlice.actions
 export default noteSlice.reducer
