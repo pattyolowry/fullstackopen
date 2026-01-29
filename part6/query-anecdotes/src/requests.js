283deab2e7b244169734349129a6aee94b3d9278
Patrick Lowry <patio.lowry@gmail.com>
2026-01-29 17:32:46 -0500

    exercise 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -7,3 +7,19 @@ export const getAnecdotes = async () => {
   }
   return await response.json()
 }
+
+export const createAnecdote = async (newAnecdote) => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newAnecdote)
+    }
+
+    const response = await fetch(baseUrl, options)
+
+    if (!response.ok) {
+        throw new Error('Failed to create note')
+    }
+
+    return await response.json()
+}
