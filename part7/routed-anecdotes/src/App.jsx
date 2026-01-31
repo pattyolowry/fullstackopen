2d22d8fabac1ea3641666d0ceb21e95f0afbf926
Patrick Lowry <patio.lowry@gmail.com>
2026-01-31 18:22:55 -0500

    exercise 7.2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import {
-  BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link, useMatch
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -21,10 +20,22 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -44,7 +55,7 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
 
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a href='https://github.com/pattyolowry/fullstackopen/tree/main/part7/routed-anecdotes'>https://github.com/pattyolowry/fullstackopen/tree/main/part7/routed-anecdotes</a> for the source code.
   </div>
 )
 
@@ -130,10 +141,14 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match 
+    ? anecdotes.find(a => a.id === Number(match.params.id))
+    : null
+
   return (
       <div>
         <h1>Software anecdotes</h1>
-        <Router>
           <div>
             <Link style={padding} to="/">home</Link>
             <Link style={padding} to="/create">create new</Link>
@@ -144,8 +159,8 @@ const App = () => {
             <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
             <Route path="/create" element={<CreateNew addNew={addNew} />} />
             <Route path="/about" element={<About />} />
+            <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
           </Routes>
-        </Router>
         <Footer />
       </div>
   )
