55bc49f3b6519addf2131f7cf7c5bf376eb822a0
Patrick Lowry <patio.lowry@gmail.com>
2026-02-15 23:14:16 -0500

    exercise 8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -128,6 +128,7 @@ const typeDefs = /* GraphQL */ `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -168,6 +169,15 @@ const resolvers = {
         });
       }
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
