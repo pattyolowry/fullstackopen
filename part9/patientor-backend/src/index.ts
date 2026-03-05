faed2d4d95e6c8bd7f02082f33e16a52b38c0448
Patrick Lowry <patio.lowry@gmail.com>
2026-03-05 16:45:41 -0500

    exercise 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnoses';
 import cors from 'cors';
 const app = express();
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+
 app.use(cors());
 app.use(express.json());
 
@@ -12,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
