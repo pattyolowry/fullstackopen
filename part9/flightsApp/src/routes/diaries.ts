a6127aa04d45707dceecef8c9254670555186493
Patrick Lowry <patio.lowry@gmail.com>
2026-03-12 10:54:59 -0400

    notes part 9c continued

--- part9/flightsApp/src/routes/diaries.ts
+++ part9/flightsApp/src/routes/diaries.ts
@@ -1,27 +1,14 @@
 import express from 'express';
 import diaryService from '../services/diaryService';
-import { Response } from 'express';
-import { NonSensitiveDiaryEntry } from "../types";
-import toNewDiaryEntry from '../utils';
+import { Request, Response, NextFunction } from 'express';
+import { NonSensitiveDiaryEntry, NewDiaryEntry, DiaryEntry } from "../types";
+import { newEntrySchema } from '../utils';
+import { z } from "zod";
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
-});
-
-router.post('/', (req, res) => {
-  try {
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
-    res.json(addedEntry);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
 });
 
 router.get('/:id', (req, res) => {
@@ -33,5 +20,29 @@ router.get('/:id', (req, res) => {
     res.sendStatus(404);
   }
 });
+
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => { 
+  try {
+    newEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post('/', newDiaryParser, (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {
+  const addedEntry = diaryService.addDiary(req.body);
+  res.json(addedEntry);
+});
+
+router.use(errorMiddleware);
 
 export default router;
--- part9/flightsApp/src/utils.ts
+++ part9/flightsApp/src/utils.ts
@@ -1,67 +1,13 @@
 import { NewDiaryEntry, Weather, Visibility } from './types';
+import { z } from "zod";
 
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
+export const newEntrySchema = z.object({
+  weather: z.enum(Weather),
+  visibility: z.enum(Visibility),
+  date: z.iso.date(),
+  comment: z.string().optional()
+});
+
+export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+  return newEntrySchema.parse(object);
 };
-
-const parseComment = (comment: unknown): string => {
-  if (!comment || !isString(comment)) {
-    throw new Error('Incorrect or missing comment');
-  }
-
-  return comment;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDate = (date: unknown): string => {
-  if (!isString(date) || !isDate(date)) {
-      throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
-
-const isWeather = (param: string): param is Weather => {
-  return Object.values(Weather).map(v => v.toString()).includes(param);
-};
-
-const parseWeather = (weather: unknown): Weather => {
-  if (!isString(weather) || !isWeather(weather)) {
-      throw new Error('Incorrect or missing weather: ' + weather);
-  }
-  return weather;
-};
-
-const isVisibility = (param: string): param is Visibility => {
-  return Object.values(Visibility).map(v => v.toString()).includes(param);
-};
-
-const parseVisibility = (visibility: unknown): Visibility => {
-  if (!isString(visibility) || !isVisibility(visibility)) {
-      throw new Error('Incorrect or missing visibility: ' + visibility);
-  }
-  return visibility;
-};
-
-const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
-  if ( !object || typeof object !== 'object' ) {
-    throw new Error('Incorrect or missing data');
-  }
-
-  if ('comment' in object && 'date' in object && 'weather' in object && 'visibility' in object)  {
-    const newEntry: NewDiaryEntry = {
-      weather: parseWeather(object.weather),
-      visibility: parseVisibility(object.visibility),
-      date: parseDate(object.date),
-      comment: parseComment(object.comment)
-    };
-
-    return newEntry;
-  }
-
-  throw new Error('Incorrect data: some fields are missing');
-};
-
-export default toNewDiaryEntry;
