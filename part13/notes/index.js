9aaa485fb869c95a34274d1f07fa35e0d0f52c70
Patrick Lowry <patio.lowry@gmail.com>
2026-03-27 14:20:03 -0400

    notes chapter 13.2 continued

--- part13/notes/index.js
+++ part13/notes/index.js
@@ -38,6 +38,7 @@ Note.init(
     modelName: "note",
   },
 );
+Note.sync();
 
 app.get("/api/notes", async (req, res) => {
   const notes = await Note.findAll();
@@ -50,6 +51,26 @@ app.post("/api/notes", async (req, res) => {
     return res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
 });
 
