9ed4fda9a1e0b9b72eaf609a20dc43a5c47fb6aa
Patrick Lowry <patio.lowry@gmail.com>
2026-03-28 14:33:56 -0400

    exercise 13.5

---
+++ part13/blogs/controllers/blogs.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const blog = await Blog.create({ ...req.body });
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Blog.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+module.exports = router;
