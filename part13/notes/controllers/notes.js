c431670b9f7fd9ccb29d0797103d7944d0dcd198
Patrick Lowry <patio.lowry@gmail.com>
2026-03-29 20:47:11 -0400

    notes chapter 13.3 continued

--- part13/notes/controllers/notes.js
+++ part13/notes/controllers/notes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
-const { Note } = require("../models");
+const { Note, User } = require("../models");
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
@@ -10,14 +12,39 @@ const noteFinder = async (req, res, next) => {
   next();
 };
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
 router.get("/", async (req, res) => {
-  const notes = await Note.findAll();
+  const notes = await Note.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
   res.json(notes);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", tokenExtractor, async (req, res) => {
   try {
-    const note = await Note.create({ ...req.body, date: new Date() });
+    const user = await User.findOne();
+    const note = await Note.create({
+      ...req.body,
+      date: new Date(),
+      userId: user.id,
+    });
     res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
