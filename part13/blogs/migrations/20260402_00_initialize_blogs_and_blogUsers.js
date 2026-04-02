847d7f978fffcb0299b390eebe8f0380f0b1102c
Patrick Lowry <patio.lowry@gmail.com>
2026-04-02 14:03:22 -0400

    exercise 13.18

---
+++ part13/blogs/migrations/20260402_00_initialize_blogs_and_blogUsers.js
@@ -0,0 +1,68 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("blog_users", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable("blogs", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.TEXT,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      blog_user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "blog_users", key: "id" },
+      },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("blogs");
+    await queryInterface.dropTable("blog_users");
+  },
+};
--- part13/blogs/models/index.js
+++ part13/blogs/models/index.js
@@ -4,9 +4,6 @@ const BlogUser = require("./user");
 BlogUser.hasMany(Blog);
 Blog.belongsTo(BlogUser);
 
-Blog.sync({ alter: true });
-BlogUser.sync({ alter: true });
-
 module.exports = {
   Blog,
   BlogUser,
