a9e0714274014467e7240bbc221c32a349e12377
Patrick Lowry <patio.lowry@gmail.com>
2026-04-02 13:46:21 -0400

    notes chapter 13.4

--- part13/notes/controllers/users.js
+++ part13/notes/controllers/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const { tokenExtractor, isAdmin } = require("../util/middleware");
 const { User, Note } = require("../models");
 
 router.get("/", async (req, res) => {
@@ -26,6 +26,22 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
   if (user) {
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
     res.json(user);
   } else {
     res.status(404).end();
