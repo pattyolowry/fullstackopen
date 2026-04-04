045612eed845fa72a3d1acf7254d9c46e22b1046
Patrick Lowry <patio.lowry@gmail.com>
2026-04-04 15:54:18 -0400

    notes chapter 13.4 continued

--- part13/notes/models/user.js
+++ part13/notes/models/user.js
@@ -34,6 +34,32 @@ User.init(
     underscored: true,
     timestamps: false,
     modelName: "user",
+    defaultScope: {
+      where: {
+        disabled: false,
+      },
+    },
+    scopes: {
+      admin: {
+        where: {
+          admin: true,
+        },
+      },
+      disabled: {
+        where: {
+          disabled: true,
+        },
+      },
+      name(value) {
+        return {
+          where: {
+            name: {
+              [Op.like]: value,
+            },
+          },
+        };
+      },
+    },
   },
 );
 
